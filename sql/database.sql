6d0717bc87c8a8e62daa7209f2ea8959d29f6a1a
Vladislav <shramru@gmail.com>
2016-03-29 01:08:52 +0300

    fix sql script

--- sql/database.sql
+++ sql/database.sql
@@ -1,3 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS `java_database` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL PRIVILEGES ON 'java_database'.* TO 'www-data'@'localhost' IDENTIFIED BY 'technopark';
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `init_database`()
 BEGIN
